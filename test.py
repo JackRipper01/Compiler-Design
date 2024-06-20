650c3367bef0718fb0e714844ffe8e70292a15fe
zahdehv <107417716+zahdehyv@users.noreply.github.com>
2024-06-19 23:29:06 -0400

    added lexer from zdv

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import math
 import random
 
-from numpy import mat
 
 PI = math.pi
 E = math.e
@@ -9,4 +8,32 @@ sin = math.sin
 cos = math.cos
 log = math.log
 
-print(sin(2 * PI) * 2 + 3 * PI / log(64, 4))
+#print(sin(2 * PI) * 2 + 3 * PI / log(64, 4))
+
+import lexer
+
+
+print("\nMIXED TEST\n")
+code = """let a = 42, let mod = a % 3 in
+    print(
+        if (mod == 0) "Magic"
+        elif (mod % 3 == 1) "Woke"
+        else "Dumb"
+    );
+"""
+lexer.hulk_lexer.input(code)
+while True:
+	tok = lexer.hulk_lexer.token()
+	print(tok)
+	if not tok: break
+
+print("\n\nARITHMETIC TEST\n")
+code = """print(
+	rand()+23.23-1e-12-
+    cos(PI+E))"""
+lexer.hulk_lexer.input(code)
+while True:
+	tok = lexer.hulk_lexer.token()
+	print(tok)
+	if not tok: break
+
