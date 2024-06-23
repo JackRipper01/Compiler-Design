914cc6a2aea314138b625a07daff029a20bc162e
JackRipper01 <francohernandezpiloto@gmail.com>
2024-06-22 22:31:39 -0400

    merging manually zlv in develop

--- myparser.py
+++ myparser.py
@@ -11,10 +11,12 @@ from lexer import tokens
 # constants
 PI = math.pi
 E = math.e
+import hulk_lexer
 
 # Build the lexer
-HL = HulkLexer()
-
+# HL = HulkLexer()
+lexer = hulk_lexer.lex.lex(module=hulk_lexer)
+lexer.parenthesisCount = 0
 # lexer = lex.lex() old lexer
 ###############################################################################################
 ###############################################################################################
@@ -35,6 +37,97 @@ precedence = (
 
 
 # region Defining the Grammatical##########################
+
+
+# region mauricio grammar
+# def p_empty(p):
+#     "empty :"
+#     pass
+
+
+# def p_program(p):
+#     "program : hl_expression"
+#     p[0] = p[1]
+
+
+# def p_hl_expression(p):
+#     """hl_expression : expression SEMI
+#     | expression_block
+#     """
+#     p[0] = p[1]
+
+
+# def p_expression_tbl(p):
+#     """expression : expression_block"""
+#     p[0] = p[1]
+
+
+# def p_expression_block(p):
+#     "expression_block : LBRACE expression_block_list RBRACE"
+#     p[0] = nc.ExpressionBlock(p[2])
+#     for i in p[2]:
+#         i.parent = p[0]
+
+
+# def p_expression_block_list(p):
+#     "expression_block_list : hl_expression expression_block_list"
+#     p[0] = []
+#     p[0].append(p[1])
+#     p[0] = p[0] + p[2]
+
+
+# def p_expression_block_list_empty(p):
+#     """expression_block_list : empty"""
+#     p[0] = []
+
+
+# def p_hl_let(p):
+#     """hl_expression : LET assign_values IN hl_expression"""
+#     p[0] = nc.Let(p[2], p[4])
+#     for i in p[2]:
+#         i.parent = p[0]
+#     p[4].parent = p[0]
+
+
+# def p_let(p):
+#     """expression : LET assign_values IN expression"""
+#     p[0] = nc.Let(p[2], p[4])
+#     for i in p[2]:
+#         i.parent = p[0]
+#     p[4].parent = p[0]
+
+
+# def p_assign_values(p):
+#     """assign_values : NAME EQUAL expression rem_assignments"""
+#     p[0] = []
+#     id = nc.ID(p[1])
+#     assign = nc.Assign(id, p[3])
+#     id.parent = assign
+#     p[3].parent = assign
+#     p[0].append(assign)
+#     p[0] = p[0] + p[4]
+
+
+# def p_rem_assignments(p):
+#     "rem_assignments : COMMA NAME EQUAL expression rem_assignments"
+#     p[0] = []
+#     id = nc.ID(p[2])
+#     assign = nc.Assign(id, p[4])
+#     id.parent = assign
+#     p[4].parent = assign
+#     p[0].append(assign)
+#     p[0] = p[0] + p[5]
+
+
+# def p_rem_assignments_empty(p):
+#     "rem_assignments : empty"
+#     p[0] = []
+
+
+# endregion
+
+
+# region my grammar
 def p_statement_expr(p):
     "statement : expression"
     p[0] = p[1]
@@ -43,6 +136,9 @@ def p_statement_expr(p):
 def p_expression_group(p):
     "expression : LPAREN expression RPAREN"
     p[0] = p[2]
+
+
+# endregion
 
 
 def p_expression_binop(p):
@@ -140,7 +236,21 @@ def p_error(p):
 
 # endregion
 
+# region mauricio parser llamacion
+# parser = yacc.yacc(start="program")
 
+# hulk_code = """let a = print(sin(10)) in {let a=5, b=6 in {print(rand()-5*3+2);
+#                             rand();}
+#                 {print(rand()-5*3+2);
+#                             rand();}
+#                             2*23+123;
+#                 {let x=2 in let a=7 in print(1+5);
+#                  print(let asd=4 in {rand();}); }
+#                 {{{print(sin((PI*(((1/2)))+PI)));}}}{{{}}} };"""
+# # hulk_code = "print(PI-E);"
+
+# AST = parser.parse(hulk_code)
+# endregion
 parser = yacc.yacc()
 
 # Generate AST
@@ -148,10 +258,10 @@ hulk_code = "print(2 + 4)"
 ast = parser.parse(hulk_code)
 
 # # semantic and type check
-ast.check()
+# ast.check()
 
 # # evaluate the AST in python code before generating the c code
-ast.eval()
+# ast.eval()
 
 
 import code_generation as cg
