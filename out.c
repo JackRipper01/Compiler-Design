9ac565243b4f9690a142bfe2d41ccd1dca9fde3f
JackRipper01 <francohernandezpiloto@gmail.com>
2024-06-24 13:49:27 -0400

    s

--- out.c
+++ out.c
@@ -4,6 +4,17 @@
 #include <string.h>
 
 int main() {
-    printf("%f\n", (strcat("Hello World!", "candela")));
+    //let a = 5,b=a+4 in print(a + b);
+    //let a=5 in let b=a+4 in print(a+b);
+    float let(float a)
+    {
+        float let(float b)
+        {
+        printf("%f\n", a+b);
+        }
+        let(a+4);
+    }
+    let(5);
     return 0;
 }
+
