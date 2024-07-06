70b306f0001426ecef99155f68034169d70d997f
hivancd <111187629+hivancd@users.noreply.github.com>
2024-07-05 22:57:56 -0500

    Halfway type checking

--- type_checking.py
+++ type_checking.py
@@ -1,184 +1,221 @@
 
 # region Type Checking
-
+from hulk_semantic_check import ScopeBuilder
 from hulk_ast import *
 
-def check_type(node: Node, infered_type=[]):
-    if isinstance(node,Assign):
-        return assign_check_type(node)
-    elif isinstance(node, Num):
-        return number_check_type(node)
-    elif isinstance(node, StringLiteral):
-        return string_check_type(node)
-    elif isinstance(node, Let):
-        let_check_type(node)
-    elif isinstance(node, Print):
-        return print_check_type(node)
-    elif isinstance(node, BinOp):
-        return binop_check_type(node)
-    elif isinstance(node, Exp):
-        return exp_check_type(node)
-    elif isinstance(node, Log):
-        return log_check_type(node)
-    elif isinstance(node, Cos):
-        return cos_check_type(node)
-    elif isinstance(node, Rand):
-        return rand_check_type(node)
-    elif isinstance(node, Pi):
-        return pi_check_type(node)
-    elif isinstance(node, E):
-        return e_check_type(node)
-    elif isinstance(node, Sin):
-        return sin_check_type(node)
-    elif isinstance(node, Sqrt):
-        return sqrt_check_type(node)
-    elif isinstance(node, FunctionDef):
-        return function_def_check_type(node)
-    elif isinstance(node, Params):
-        return params_check_type(node)
-    elif isinstance(node, FunctionCall):
-        return function_call_check_type(node)
-    elif isinstance(node, ExpressionBlock):
-        return expression_block_check_type(node)
-    elif isinstance(node, TrueLiteral):
-        return true_check_type(node)
-    elif isinstance(node, FalseLiteral):
-        return false_check_type(node)
+class Type_Check():
     
-
-def true_check_type(node: TrueLiteral,infered_type=[]):
-    return check_and_ret(node,'bool',infered_type)
-  
-def false_check_type(node: FalseLiteral,infered_type=[]):
-    return check_and_ret(node,'bool',infered_type)  
-
-# Not understand ExpressionBlock class
-def expression_block_check_type(node: ExpressionBlock,infered_type=[]):
-    pass
+    def __innit__(self,ast:Program):
+        self.context= ScopeBuilder.get_global_definitions(ast)
+        
+    @classmethod
+    def check_type(cls,node: Node, infered_type=[]):
+        if isinstance(node,Assign):
+            return cls.assign_check_type(node)
+        elif isinstance(node, Num):
+            return cls.number_check_type(node)
+        elif isinstance(node, StringLiteral):
+            return cls.string_check_type(node)
+        elif isinstance(node, Let):
+            return cls.let_check_type(node)
+        elif isinstance(node, Print):
+            return cls.print_check_type(node)
+        elif isinstance(node, BinOp):
+            return cls.binop_check_type(node)
+        elif isinstance(node, Exp):
+            return cls.exp_check_type(node)
+        elif isinstance(node, Log):
+            return cls.log_check_type(node)
+        elif isinstance(node, Cos):
+            return cls.cos_check_type(node)
+        elif isinstance(node, Rand):
+            return cls.rand_check_type(node)
+        elif isinstance(node, Pi):
+            return cls.pi_check_type(node)
+        elif isinstance(node, E):
+            return cls.e_check_type(node)
+        elif isinstance(node, Sin):
+            return cls.sin_check_type(node)
+        elif isinstance(node, Sqrt):
+            return cls.sqrt_check_type(node)
+        elif isinstance(node, FunctionDef):
+            return cls.function_def_check_type(node)
+        elif isinstance(node, Params):
+            return cls.params_check_type(node)
+        elif isinstance(node, FunctionCall):
+            return cls.function_call_check_type(node)
+        elif isinstance(node, ExpressionBlock):
+            return cls.expression_block_check_type(node)
+        elif isinstance(node, TrueLiteral):
+            return cls.true_check_type(node)
+        elif isinstance(node, FalseLiteral):
+            return cls.false_check_type(node)
+        elif isinstance(node,If):
+            return cls.if_check_type(node)
+        elif isinstance(node,Case):
+            return cls.case_check_type(node)
+        elif isinstance(node, While):
+            return cls.while_check_type(node)
+        elif isinstance(node, For):
+            return cls.for_check_type(node)
+       
+       
+    # !!!! Type check that iterable is iterable
+    # How are we treating iterables? 
+    def for_check_type(node: For, infered_type=[]):
+        body_type = Type_Check.check_type(node.body)
+        return Type_Check.check_and_ret(node,body_type,infered_type)
     
-# Check that function is defined before using it
-# How to a FunctionDef node to check params types
-def function_call_check_type(node: FunctionCall,infered_type=[]):
-    # func_id = node.func_id
-    # params=node.params
-    pass
+    def while_check_type(node: While, infered_type=[]):
+        condition_type = Type_Check.check_type(node.condition,['bool'])
+        body_type = Type_Check.check_type(node.body)
+        return Type_Check.check_and_ret(node,body_type,infered_type)
     
-# Still need to enforce params type in a function
-def params_check_type(node: Params,infered_type=[]):
-    static_type= []
-    for e in nodes.param_list: 
-        static_type.append(check_type(e))
+    def case_check_type(node: Case, infered_type=[]):
+        condition_type = Type_Check.check_type(node.condition,['bool'])
+        body_type = Type_Check.check_type(node.body)
+        return Type_Check.check_and_ret(node,body_type,infered_type)
+        
+    # To do after Case
+    def if_check_type(node: If, infered_type=[]):
+        pass
+        
+    # Not understand ExpressionBlock class
+    def expression_block_check_type(node: ExpressionBlock,infered_type=[]):
+        pass
 
-# Clase para citar variables declaradas?
-# Si existe ver forma de usarla, si no crear una variable contexto para el chequeo de tipos
-def function_def_check_type(node: FunctionDef, infered_type=[]):
-    id_type= check_type(node.func_id)
-    params_type= check_type(node.params)
-    infers=[]
-    if id_type: infers=[id_type]
-    static_type= check_type(node.body, infers)
-    return check_and_ret(node,static_type,infers)
+    # Check that function is defined before using it
+    # How to a FunctionDef node to check params types !!!!
+    def function_call_check_type(node: FunctionCall,infered_type=[]):
+        # func_id = node.func_id
+        # params=node.params
+        input_params= Type_Check.check_type(node.params)
+        
+        pass
 
-def sqrt_check_type(node: Sqrt, infered_type=[]):
-    static_type= 'number'
-    return check_and_ret(node, static_type, infered_type)
+    def params_check_type(node: Params,infered_type=[]):
+        static_type= []
+        for e in nodes.param_list: 
+            static_type.append(Type_Check.check_type(e))
+        return Type_Check.check_and_ret(node,static_type,[])
 
-def sin_check_type(node: Sin, infered_type=[]):
-    static_type= 'number'
-    return check_and_ret(node, static_type, infered_type)
+    # Clase para citar variables declaradas?
+    # Si existe ver forma de usarla, si no crear una variable contexto para el chequeo de tipos
+    def function_def_check_type(node: FunctionDef, infered_type=[]):
+        id_type= Type_Check.check_type(node.func_id)
+        params_type= Type_Check.check_type(node.params)
+        infers=[]
+        if id_type: infers=[id_type]
+        static_type= Type_Check.check_type(node.body, infers)
+        return Type_Check.check_and_ret(node,static_type,infers)
 
-def e_check_type(node: E, infered_type=[]):
-    static_type= 'number'
-    return check_and_ret(node, static_type, infered_type)
+    def true_check_type(node: TrueLiteral,infered_type=[]):
+        return Type_Check.check_and_ret(node,'bool',infered_type)
+    
+    def false_check_type(node: FalseLiteral,infered_type=[]):
+        return Type_Check.check_and_ret(node,'bool',infered_type)  
+    
+    def sqrt_check_type(node: Sqrt, infered_type=[]):
+        static_type= 'number'
+        return Type_Check.check_and_ret(node, static_type, infered_type)
 
-def pi_check_type(node: Pi, infered_type=[]):
-    static_type= 'number'
-    return check_and_ret(node, static_type, infered_type)
+    def sin_check_type(node: Sin, infered_type=[]):
+        static_type= 'number'
+        return Type_Check.check_and_ret(node, static_type, infered_type)
 
-def rand_check_type(node: Rand, infered_types=[]):
-    static_type= 'number'
-    return check_and_ret(node,static_type,infered_types)
+    def e_check_type(node: E, infered_type=[]):
+        static_type= 'number'
+        return Type_Check.check_and_ret(node, static_type, infered_type)
 
-def cos_check_type(node: Cos, infered_types=[]):
-    static_type= check_type(node.value, ['number'])
-    return check_and_ret(node,static_type,infered_types)
+    def pi_check_type(node: Pi, infered_type=[]):
+        static_type= 'number'
+        return Type_Check.check_and_ret(node, static_type, infered_type)
 
-def log_check_type(node: Log, infered_types=[]):
-    static_type= check_type(node.value, ['number'])
-    base = check_type(node.base, ['number'])
-    return check_and_ret(node,static_type,infered_types)
+    def rand_check_type(node: Rand, infered_types=[]):
+        static_type= 'number'
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-def exp_check_type(node: Exp,infered_types=[]):
-    static_type= check_type(node.value, ['number'])
-    return check_and_ret(node,static_type,infered_types)
+    def cos_check_type(node: Cos, infered_types=[]):
+        static_type= Type_Check.check_type(node.value, ['number'])
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-def binop_check_type(node: BinOp,infered_types=[]):
-    left_type = check_type(node.left)
-    right_type = check_type(node.right)
-    static_type=''
-    # Check that the types are valid for the operation
-    if node.op in ["+", "-", "*", "/", "^", "**"]:
-        if left_type != "number" or right_type != "number":
-            raise TypeError(f"Invalid type for operation: {node.op}")
-        else: static_type= 'number'
-    if node.op == "@":
-        if (
-            left_type != "string"
-            and left_type != "number"
-            or right_type != "string"
-            and right_type != "number"
-        ):
-            raise TypeError(f"Invalid type for operation: {left_type}")
-        else: static_type= 'string'
-    return check_and_ret(node,static_type,infered_types)
+    def log_check_type(node: Log, infered_types=[]):
+        static_type= Type_Check.check_type(node.value, ['number'])
+        base = Type_Check.check_type(node.base, ['number'])
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-# !!!!check_type methods Not revised completely from this Point
+    def exp_check_type(node: Exp,infered_types=[]):
+        static_type= Type_Check.check_type(node.value, ['number'])
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-def print_check_type(node: Print,infered_types=[]):
-    static_type= "void"
-    return check_and_ret(node,static_type,infered_types)
+    def binop_check_type(node: BinOp,infered_types=[]):
+        left_type = Type_Check.check_type(node.left)
+        right_type = Type_Check.check_type(node.right)
+        static_type=''
+        # Check that the types are valid for the operation
+        if node.op in ["+", "-", "*", "/", "^", "**"]:
+            if left_type != "number" or right_type != "number":
+                raise TypeError(f"Invalid type for operation: {node.op}")
+            else: static_type= 'number'
+        if node.op == "@":
+            if (
+                left_type != "string"
+                and left_type != "number"
+                or right_type != "string"
+                and right_type != "number"
+            ):
+                raise TypeError(f"Invalid type for operation: {left_type}")
+            else: static_type= 'string'
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-def let_check_type(node: Let, infered_types=[]):
-    static_type= check_type(node.body)
-    check_and_ret(node,static_type,infered_types)
+    # !!!!check_type methods Not revised completely from this Point
 
-def assign_check_type(node: Assign,infered_types=[]):
-    # print('in assign_check_type')
-    declared_type = check_type(node.name)
-    type = check_type(node.value)
-    if declared_type != '':
-        if (type != declared_type):
-            # Exception here!!!!!!!!!!
-            raise TypeError('Declared type is diferent from the type of the value given.')
-    node.name.annotated_type = type
-    return type
+    def print_check_type(node: Print,infered_types=[]):
+        static_type= "void"
+        return Type_Check.check_and_ret(node,static_type,infered_types)
 
-def id_check_type(node:ID,infered_types=[]):
-    return node.annotated_type
+    def let_check_type(node: Let, infered_types=[]):
+        static_type= Type_Check.check_type(node.body)
+        Type_Check.check_and_ret(node,static_type,infered_types)
 
-def number_check_type(node: Num,infered_types=[]):
-    node.static_type= 'number'
-    return 'number'
+    def assign_check_type(node: Assign,infered_types=[]):
+        # print('in assign_check_type')
+        declared_type = Type_Check.check_type(node.name)
+        type = Type_Check.check_type(node.value)
+        if declared_type != '':
+            if (type != declared_type):
+                # Exception here!!!!!!!!!!
+                raise TypeError('Declared type is diferent from the type of the value given.')
+        node.name.annotated_type = type
+        return type
 
-def string_check_type(node: StringLiteral,infered_types=[]):
-    node.static_type= 'string'
-    return 'string'
+    def id_check_type(node:ID,infered_types=[]):
+        return node.annotated_type
+
+    def number_check_type(node: Num,infered_types=[]):
+        node.static_type= 'number'
+        return 'number'
+
+    def string_check_type(node: StringLiteral,infered_types=[]):
+        node.static_type= 'string'
+        return 'string'
 
 
-# endregion
+    # endregion
+    
+    def check_inference(type_val,infers: list):
+            asserted=False
+            for e in infers:
+                if(e == type_val):
+                    asserted= True
+            if not asserted or len(infers)==0:
+                raise Exception(f'The Type of the object cannot be of type: {type_val}')
+    
+    def check_and_ret(node,static_type,infers):
+        Type_Check.check_inference(static_type, infers)
+        node.static_type= static_type
+        return static_type
 
-def check_and_ret(node,static_type,infers):
-    check_inference(static_type, infers)
-    node.static_type= static_type
-    return static_type
-
-def check_inference(type_val,infers: list):
-    asserted=False
-    for e in infers:
-        if(e == type_val):
-            asserted= True
-    if not asserted or len(infers)==0:
-        raise Exception(f'The Type of the object cannot be of type: {type_val}')
     
         
         
